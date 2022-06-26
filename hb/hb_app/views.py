ccc5e4106e44f43a623cecf11ac4b42a01bdd0b4
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-26 19:27:07 -0400

    Adding urls.py to app and creating first Hello World view

--- hb/hb_app/views.py
+++ hb/hb_app/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class TestView(View):
+
+    def get(self, *args, **kwargs):
+        return HttpResponse("Hello World")
