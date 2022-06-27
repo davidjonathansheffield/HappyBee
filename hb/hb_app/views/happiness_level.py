62a47efe5cf1b288bf14be4af51d6e3ed002ea60
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 09:54:56 -0400

    Adding preliminary Happiness Level View

---
+++ hb/hb_app/views/happiness_level.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+from rest_framework.response import Response
+
+from .base import BaseAuthAPIView
+
+from hb_app.models import HappinessLevel
+
+
+class SubmitHappinessAPIView(BaseAuthAPIView):
+
+    @staticmethod
+    def get_member(request):
+        try:
+            return request.user.member
+        except AttributeError:
+            member = None
+
+        return member
+
+    def post(self, request):
+        member = self.get_member(request)
+        if member is None:
+            return Response({"error": "User is not on a team. Please request an admin place this user on a team."})
+
+        happiness_level = HappinessLevel.objects.daily_happiness_level(
+            team_member_id=member.id,
+        )
+
+        return Response({"derp": "derp"})
+
+
+__all__ = (
+    'SubmitHappinessAPIView',
+)
