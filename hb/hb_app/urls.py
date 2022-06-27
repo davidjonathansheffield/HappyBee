6eb4b7c5eb7320e5cb2e6e3fcb54758ad952567e
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 09:55:26 -0400

    Adding happiness submission view endpoint

--- hb/hb_app/urls.py
+++ hb/hb_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
+from hb_app.views import *
+
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
+    path('happiness/submit/', SubmitHappinessAPIView.as_view()),
 ]
