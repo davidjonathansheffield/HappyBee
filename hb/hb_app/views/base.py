e2718dec5667b8d94e2595bb28f9267d32000452
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 09:52:45 -0400

    Adding Adding BaseAPIView and BaseAPIAuthView

--- hb/hb_app/views/base.py
+++ hb/hb_app/views/base.py
@@ -1,11 +1,24 @@
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class BaseAPIView(APIView):
-    pass
+    """
+    A base interface for all APIViews.
+    """
+    authentication_classes = [TokenAuthentication]
+
+
+class BaseAuthAPIView(BaseAPIView):
+    """
+    A base authentication required APIView.
+    """
+    permission_classes = [IsAuthenticated]
 
 
 __all__ = (
     'BaseAPIView',
+    'BaseAuthAPIView',
 )
