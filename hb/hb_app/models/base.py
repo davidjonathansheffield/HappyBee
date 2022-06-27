7c205958234f118acaf45d290595696fbd592199
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 09:52:11 -0400

    Adding BaseManager

--- hb/hb_app/models/base.py
+++ hb/hb_app/models/base.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class BaseManager(models.Manager):
+    pass
+
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
