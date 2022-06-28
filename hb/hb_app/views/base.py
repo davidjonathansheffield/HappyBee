0bf7938aa12bd2432547a3324de3623955da86be
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 20:46:53 -0400

    Enhancing BaseAPIView

--- hb/hb_app/views/base.py
+++ hb/hb_app/views/base.py
@@ -1,5 +1,6 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,6 +10,19 @@ class BaseAPIView(APIView):
     A base interface for all APIViews.
     """
     authentication_classes = [TokenAuthentication]
+    renderer_classes = [JSONRenderer]
+    serializer_class = None
+
+    def post(self, request):
+        if self.serializer_class is None:
+            raise NotImplementedError
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.requesting_user = request.user
+        if serializer.is_valid():
+            return Response(serializer.save())
+
+        raise ValueError("Invalid arguments passed to API Endpoint.")
 
 
 class BaseAuthAPIView(BaseAPIView):
