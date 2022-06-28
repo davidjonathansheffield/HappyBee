9c8b6bddde944dfc51b14e6c2126b8a49eec9ba5
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 20:47:21 -0400

    Moving validation and creation logic to serializer

--- hb/hb_app/serializers/happiness_level.py
+++ hb/hb_app/serializers/happiness_level.py
@@ -1,5 +1,45 @@
+from rest_framework import serializers
+
+from hb_app.models import HappinessLevel
 from .base import BaseSerializer
 
 
-class HappinessLevelSerializer(BaseSerializer):
-    pass
+class SubmitHappinessSerializer(BaseSerializer):
+    rating = serializers.IntegerField()
+
+    @staticmethod
+    def get_member(requesting_user):
+        try:
+            return requesting_user.member
+        except AttributeError:
+            member = None
+
+        return member
+
+    def validate(self, attrs):
+        member = self.get_member(self.requesting_user)
+
+        if member is None:
+            raise ValueError("User is not on a team. Please request an admin place this user on a team.")
+
+        attrs['team_member_id'] = member.id
+        return attrs
+
+    def create(self, validated_data):
+        try:
+            happiness_level = HappinessLevel.objects.record(
+                team_member_id=validated_data.get('team_member_id'),
+                rating=validated_data.get('rating'),
+            )
+        except ValueError as e:
+            return {
+                'errors': e.args,
+                'success': False,
+            }
+
+        return {
+            'created_at': happiness_level.created_at,
+            'rating': happiness_level.rating,
+            'success': True,
+            'team_member_id': happiness_level.team_member_id,
+        }
