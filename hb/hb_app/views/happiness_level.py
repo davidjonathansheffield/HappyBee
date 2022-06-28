8917819711b04f83b026f248d1731f64eb89cfdf
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 20:47:33 -0400

    Taking logic out of view

--- hb/hb_app/views/happiness_level.py
+++ hb/hb_app/views/happiness_level.py
@@ -1,33 +1,9 @@
-from dataclasses import dataclass
-
-from rest_framework.response import Response
-
+from serializers.happiness_level import SubmitHappinessSerializer
 from .base import BaseAuthAPIView
-
-from hb_app.models import HappinessLevel
 
 
 class SubmitHappinessAPIView(BaseAuthAPIView):
-
-    @staticmethod
-    def get_member(request):
-        try:
-            return request.user.member
-        except AttributeError:
-            member = None
-
-        return member
-
-    def post(self, request):
-        member = self.get_member(request)
-        if member is None:
-            return Response({"error": "User is not on a team. Please request an admin place this user on a team."})
-
-        happiness_level = HappinessLevel.objects.record(
-            team_member_id=member.id,
-        )
-
-        return Response({"derp": "derp"})
+    serializer_class = SubmitHappinessSerializer
 
 
 __all__ = (
