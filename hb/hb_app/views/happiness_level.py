8964ce53d027e3f234b65d69495844961358a37c
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-06-27 23:03:16 -0400

    Adding allowable requests to happiness_level.py

--- hb/hb_app/views/happiness_level.py
+++ hb/hb_app/views/happiness_level.py
@@ -1,8 +1,10 @@
+from hb_app.enums import RequestMethodEnum
 from serializers.happiness_level import SubmitHappinessSerializer
 from .base import BaseAuthAPIView
 
 
 class SubmitHappinessAPIView(BaseAuthAPIView):
+    allowable_request_methods = {RequestMethodEnum.POST}
     serializer_class = SubmitHappinessSerializer
 
 
